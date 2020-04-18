e1676a8500e7b2047a2bace42632a451232b6a62
Jakub Kiermasz <kiermasz.k@gmail.com>
2020-04-18 11:47:27 +0200

    Add application to workspace

---
+++ HapticStudy/HapticStudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HapticStudy
+//
+//  Created by jaki on 18/04/2020.
+//  Copyright © 2020 Jakub Kiermasz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
